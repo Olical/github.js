5fe7a2976d2d524eec6f823f61d668bcaab75cb9
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-09 16:28:04 +0000

    Added main module loading to the bottom of the first function wrapper

---
+++ makefile
@@ -0,0 +1,4 @@
+default: all
+
+github.js:
+	
--- src/github.js/main.js
+++ src/github.js/main.js
@@ -1,13 +1,13 @@
-define(function() {
+(function(exports) {
 	// Initialise the class
 	function GitHub(options) {
 		// Set any passed options
 		this.setOptions(options);
 	}
 	
+	// Expose the class
+	exports.GitHub = GitHub;
+	
 	// Load main modules
 	require(['options', 'api']);
-	
-	// Expose the class
-	window.GitHub = GitHub;
-});
+}(this));
