d8666ddfc8684c8431ab8f2f72f64749c8e2b396
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-09 16:01:14 +0000

    Added object merging for settings

--- src/github.js/options.js
+++ src/github.js/options.js
@@ -5,9 +5,21 @@ define(['api'], function() {
 	 * @param {Object} options Options to be set
 	 */
 	GitHub.implement('setOptions', function(options) {
+		// Initialise any required variables
+		var i = null;
+		
 		// If there is no options object just use this one or a blank object
 		if(!this.options) {
 			this.options = options || {};
+		}
+		else {
+			// So it does exist
+			// We now loop over the properties merging the objects
+			for(i in options) {
+				if(options.hasOwnProperty(i)) {
+					this.options[i] = options[i];
+				}
+			}
 		}
 	});
 });
