4f46776c6a00406c0dcd5f123a1a7e63a130a85d
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-12 12:21:59 +0000

    Built

--- github.js
+++ github.js
@@ -1,5 +1,13 @@
+/**
+ * GitHub JavaScript API v0.0.0
+ * https://github.com/Wolfy87/github.js
+ */
 (function(exports) {
-	// Initialise the class
+	/**
+	 * GitHub API v3 class
+	 * 
+	 * @param {Object} options List of options to pass to the class
+	 */
 	function GitHub(options) {
 		// Set any passed options
 		this.setOptions(options);
