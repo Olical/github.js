f9f2d01e8667512049c8706220efbb6f6527ac04
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-12 21:10:29 +0000

    Combined the source files

--- github.js
+++ github.js
@@ -26,7 +26,7 @@ GitHub.implement = function(key, value) {
  * 
  * @param {Object} options Options to be set
  */
-GitHub.implement('setOptions', function(options) {
+function setOptions(options) {
 	// Initialise any required variables
 	var i = null;
 	
@@ -43,6 +43,70 @@ GitHub.implement('setOptions', function(options) {
 			}
 		}
 	}
-});	// Expose the class
+}
+
+// Implement setOptions in the GitHub class
+GitHub.implement('setOptions', setOptions);/**
+ * HTTP request class
+ * Makes HTTP requests easy
+ * 
+ * @param {Object} options List of options to pass to the class
+ */
+function HTTPRequest(options) {
+	// Set the default options
+	this.setOptions({
+		method: 'GET',
+		async: true,
+		url: null,
+		user: null,
+		password: null,
+		data: null
+	});
+	
+	// Set the users passed options
+	this.setOptions(options);
+}
+
+/**
+ * Sets the requests options
+ * 
+ * @param {Object} options Your list of options
+ */
+HTTPRequest.prototype.setOptions = setOptions;
+
+/**
+ * Sends the HTTP request configured in the options object
+ * 
+ * @param {Function} callback The function to send the results to if asyncronous
+ * @returns {String} The results will be returned if it is not an asyncronous request
+ */
+HTTPRequest.prototype.send = function(callback) {
+	// Initialise variables
+	var self = this,
+		request = null;
+	
+	// Initialise the request
+	request = new XMLHttpRequest();
+	request.open(this.options.method, this.options.url, this.options.async, this.options.user, this.options.password);
+	
+	if(this.options.async && callback) {
+		// Add the event listeners
+		request.addEventListener('readystatechange', function() {
+			// Check if the request is done
+			if(request.readyState === 4 && request.status === 200) {
+				// It is, send the data to the callback
+				callback.call(null, request.responseText);
+			}
+		});
+	}
+	
+	// Send the request
+	request.send(this.options.data);
+	
+	// If it is not an async request, send back the results instantly
+	if(!this.options.async) {
+		return request.responseText;
+	}
+};	// Expose the class
 	exports.GitHub = GitHub;
 }(this));
