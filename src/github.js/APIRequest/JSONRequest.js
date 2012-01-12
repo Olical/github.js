3e56bea18d7c829330214bb01d43a2733e5de0ce
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-12 21:26:29 +0000

    Finished the JSONRequest object

---
+++ src/github.js/APIRequest/JSONRequest.js
@@ -0,0 +1,50 @@
+/**
+ * JSON request class
+ * Allows you to load and parse JSON easily
+ * Extends the HTTPRequest class
+ * 
+ * @param {Object} options List of options to pass to the class
+ */
+function JSONRequest(options) {
+	HTTPRequest.call(this, options);
+}
+
+// Extend the HTTPRequest class
+JSONRequest.prototype = HTTPRequest.prototype;
+
+/**
+ * Handles the response from a HTTP request
+ * This expects JSON and will return the decoded JSON
+ * 
+ * @param {String} response The HTTP response in JSON
+ * @returns {Mixed} The decoded JSON, usually an array or object
+ */
+JSONRequest.prototype.handleResponse = function(response) {
+	// Decode and return the data
+	return JSON.parse(response);
+};
+
+/**
+ * Sends the HTTP request configured in the options object
+ * Will decode the response as JSON
+ * 
+ * @param {Function} callback The function to send the results to if asyncronous
+ * @returns {String} The results will be returned if it is not an asyncronous request
+ */
+JSONRequest.prototype.send = function(callback) {
+	// Initialise variables
+	var self = this;
+	
+	// Perform this differently depending on the async option
+	if(this.options.async) {
+		// Async, use a callback
+		HTTPRequest.prototype.send.call(this, function(response) {
+			// Decode the response and send it to the callback
+			callback.call(null, self.handleResponse(response));
+		});
+	}
+	else {
+		// Non async, so we handle the returned data and pass it to the callback
+		callback.call(null, self.handleResponse(HTTPRequest.prototype.send.call(this)));
+	}
+};
