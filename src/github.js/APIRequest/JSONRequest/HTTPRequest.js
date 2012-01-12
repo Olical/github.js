55dcfceeb786e404ae511e6129d97c5f5bcabdcc
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-12 20:53:22 +0000

    Finished the HTTPRequest class.

---
+++ src/github.js/APIRequest/JSONRequest/HTTPRequest.js
@@ -0,0 +1,62 @@
+/**
+ * HTTP request class
+ * Makes HTTP requests easy
+ * 
+ * @param {Object} options List of options to pass to the class
+ */
+function HTTPRequest(options) {
+	// Set the default options
+	this.setOptions({
+		method: 'GET',
+		async: true,
+		url: null,
+		user: null,
+		password: null,
+		data: null
+	});
+	
+	// Set the users passed options
+	this.setOptions(options);
+}
+
+/**
+ * Sets the requests options
+ * 
+ * @param {Object} options Your list of options
+ */
+HTTPRequest.prototype.setOptions = setOptions;
+
+/**
+ * Sends the HTTP request configured in the options object
+ * 
+ * @param {Function} callback The function to send the results to if asyncronous
+ * @returns {String} The results will be returned if it is not an asyncronous request
+ */
+HTTPRequest.prototype.send = function(callback) {
+	// Initialise variables
+	var self = this,
+		request = null;
+	
+	// Initialise the request
+	request = new XMLHttpRequest();
+	request.open(this.options.method, this.options.url, this.options.async, this.options.user, this.options.password);
+	
+	if(this.options.async && callback) {
+		// Add the event listeners
+		request.addEventListener('readystatechange', function() {
+			// Check if the request is done
+			if(request.readyState === 4 && request.status === 200) {
+				// It is, send the data to the callback
+				callback.call(null, request.responseText);
+			}
+		});
+	}
+	
+	// Send the request
+	request.send(this.options.data);
+	
+	// If it is not an async request, send back the results instantly
+	if(!this.options.async) {
+		return request.responseText;
+	}
+};
