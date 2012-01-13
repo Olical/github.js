f8413b32d7d08a61c79bb38c18ad9d8ab4068da4
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-13 10:51:31 +0000

    Added the get function. Simplifies using the HTTPRequest class

---
+++ src/github.js/api/get.js
@@ -0,0 +1,33 @@
+/**
+ * Makes a request using the APIRequest class
+ * 
+ * @param {Object} requestOptions Options object for the HTTPRequest
+ * @param {Object} apiObject Options that are indirectly passed to the HTTPRequest, should contain user and password if required
+ * @param {Function} callback Where to pass the results to, optional
+ * @returns {Mixed} The decoded JSON results from the request
+ */
+function get(requestOptions, apiOptions, callback) {
+	// Set up the request
+	var request = new APIRequest({
+		async: (callback) ? true : false
+	});
+	
+	request.setOptions(requestOptions);
+	
+	if(apiOptions.user && apiOptions.password) {
+		request.setOptions({
+			user: apiOptions.user,
+			password: apiOptions.password
+		});
+	}
+	
+	if(callback) {
+		request.send(callback);
+	}
+	else {
+		return request.send();
+	}
+}
+
+// Expose the get function in the API
+GitHub.implement('get', get);
