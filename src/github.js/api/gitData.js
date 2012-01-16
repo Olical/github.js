a2777b75f60d28385d382a065e8f6de96849cb4f
Oliver Caldwell <oliver@flowdev.co.uk>
2012-01-16 21:12:59 +0000

    Began the git data API. Currently supports blobs.

---
+++ src/github.js/api/gitData.js
@@ -0,0 +1,46 @@
+// API class for interacting with Git data on GitHub
+var gitDataApi = new GitHub.APIObject();
+
+/**
+ * Gets the content and encoding of a blob
+ * 
+ * @param {String} user The owner of the repository
+ * @param {String} repo The repositories name
+ * @param {String} sha The sha of the blob
+ * @param {Function} callback If passed it will be come an async request. Results will be passed to this
+ * @returns {Mixed} The decoded JSON response if you did not pass a callback
+ */
+gitDataApi.getBlob = function(user, repo, sha, callback) {
+	return this.instance.get({
+		urlTemplate: '/repos/${user}/${repo}/git/blobs/${sha}',
+		urlData: {
+			user: user,
+			repo: repo,
+			sha: sha
+		}
+	}, callback);
+};
+
+/**
+ * Creates a blob with your specified encoding and content
+ * 
+ * @param {String} user The owner of the repository
+ * @param {String} repo The repositories name
+ * @param {Object} settings An object containing your new blobs encoding and content 
+ * @param {Function} callback If passed it will be come an async request. Results will be passed to this
+ * @returns {Mixed} The decoded JSON response if you did not pass a callback
+ */
+gitDataApi.createBlob = function(user, repo, settings, callback) {
+	return this.instance.get({
+		urlTemplate: '/repos/${user}/${repo}/git/blobs',
+		urlData: {
+			user: user,
+			repo: repo
+		},
+		data: settings,
+		method: 'POST'
+	}, callback);
+};
+
+// Register the API
+GitHub.implement('gists', gistsApi);
